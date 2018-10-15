5b3daf4c8843cada3240c1cbba41fc452ab9e2b8
Dhia Hadj Amor <dhia.ha1995@gmail.com>
2018-10-15 01:30:49 +0100

    final

--- Internship Task/time-tracker/config/database.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-const url = 'mongodb://mongo:27017/tracker'
-
-mongoose.connect(url, { useNewUrlParser: true }).then(() => {
-    console.log('Connected to mongoDB')
-}).catch(e => {
-    console.log('Error while DB connecting');
-    console.log(e);
-});
--- Internship Task/time-tracker/config/routes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-const router = express.Router()
-import { create, get } from '../components/task/taskController'
-
-  router.get('/tasks', get)
-  router.post('/task', create)
-
-
-export default router
